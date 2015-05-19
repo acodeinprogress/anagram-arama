e04e800cbfa661569ad052464e96da5aae52396e
Sharon Siegel <sjsieg830@gmail.com>
2015-05-19 13:09:12 -0600

    Reverse words now supports 3 letter words. Doesn't work yet. Infinte loop.

--- reverse_letters.rb
+++ reverse_letters.rb
@@ -1,7 +1,24 @@
 def reverse_letters(word)
-    anagrams = []
-    (-1..0).each do |index|
-        anagrams.push(word[index])
+    if word.length == 2
+        anagrams = []
+        (-1..0).each do |index|
+            anagrams.push(word[index])
+        end
+        return anagrams
     end
-    return anagrams
+
+    if word.length == 3
+        anagrams = []
+        new_word = ''
+        original_word = word
+        while new_word != original_word
+            new_word = word
+            anagrams.push(new_word)
+            new_word = word[0] + word[2] + word[1]
+            anagrams.push(new_word)
+            word = word[1] + word[2] + word[0]
+        end
+    end
 end
+
+puts reverse_letters("dog")
