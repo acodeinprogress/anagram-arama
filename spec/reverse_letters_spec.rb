09fafba28c13f842b0f1e4ca852f3da9d39cb515
Sharon Siegel <sjsieg830@gmail.com>
2015-05-21 20:55:51 -0600

    Altered spec files so that they produced words:w rather than arrays of letter.

--- spec/reverse_letters_spec.rb
+++ spec/reverse_letters_spec.rb
@@ -2,20 +2,10 @@ require 'rspec'
 require_relative '../reverse_letters.rb'
 
 describe "#reverse_letters" do
-  let(:letters) { ['a','b'] }
-
-  it "takes an array of letters" do
-    expect {
-      reverse_letters(letters)
-    }.not_to raise_error
-  end
-
-  it "returns an array of letters" do
-    expect(reverse_letters(letters)).to be_a(Array)
-  end
+  let(:letters) { 'ab' }
 
   it "returns the letters in reverse" do
-    expect(reverse_letters(letters)).to eq(['b','a'])
+    expect(reverse_letters(letters)).to eq('ba')
   end
 
   it "returns an array of length 2" do
