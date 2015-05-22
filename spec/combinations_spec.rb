8a30681caeab72c7b4a7cd735a74ef3fd99a81a1
Sharon Siegel <sjsieg830@gmail.com>
2015-05-22 16:42:05 -0600

    Corrected spec to match the output I expect.

--- spec/combinations_spec.rb
+++ spec/combinations_spec.rb
@@ -10,6 +10,6 @@ describe 'Our Anagrams App' do
   
   it 'displays combinations for a word on a get to /anagrams/:word' do
     get("/anagrams/cat")
-    expect(last_response.body).to include("cat", "atc", "tca", "act", "cta", "tac")
+    expect(last_response.body).to include("CAT", "ATC", "TCA", "ACT", "CTA", "TAC")
   end  
 end
