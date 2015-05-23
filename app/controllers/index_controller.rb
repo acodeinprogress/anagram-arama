7e99f52dfdc1587bbd1e11bc27834a502006f138
Sharon Siegel <sjsieg830@gmail.com>
2015-05-23 00:31:05 -0600

    Checks for 2-3 letter valid words. Shows error if not true.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,31 @@
 require 'sinatra' 
 
+def three_letters?(word)
+    if params[:word].length > 3 || params[:word].length < 2
+        false
+    else
+        true
+    end
+end
+
+def distinct_letters?(input)
+    letter_array = input.chars
+    unique_letters = input.split("").uniq
+    if unique_letters.length < letter_array.length
+        false
+    else
+        true
+    end
+end
+
+def valid_input?(input)
+    if three_letters?(input) && distinct_letters?(input)
+        true
+    else
+        false
+    end
+end
+
 get '/' do
     erb :index
 end
@@ -12,5 +38,10 @@ end
 
 post '/' do
     @word = params[:word]
-    redirect "/anagrams/#{@word}"
+    if valid_input?(@word)
+        redirect "/anagrams/#{@word}"
+    else
+        @error = 'Wrong input. Please enter a 2-3 letter word'
+        erb :index
+    end
 end
