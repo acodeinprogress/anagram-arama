9d5003555566b5e3ecd0dc200cd57e80dc239b1b
Sharon Siegel <sjsieg830@gmail.com>
2015-05-24 11:03:54 -0600

    Only anagrams that are in dictionary show on list.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -22,7 +22,8 @@ end
 
 get '/anagrams/:word' do
     @word = params[:word]
-    @anagrams = Word.find_anagram(@word)
+    alphabetized_string = @word.chars.sort.join
+    @anagrams = Word.where("letters=?", alphabetized_string)
     erb :show
 end
 
